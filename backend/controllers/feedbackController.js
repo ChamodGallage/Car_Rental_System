6726d174f829ff95a2b644db6a0f838a731d0226
PiyumiImashi <it22337580@my.sliit.lk>
2025-04-30 01:21:05 +0530

    Feedback Form  Validate part

--- backend/controllers/feedbackController.js
+++ backend/controllers/feedbackController.js
@@ -11,10 +11,11 @@ const addFeedback = asyncHandler(async (req, res) => {
   const cusname = req.user.name;
 
   // Validate age
-  if (!Number.isInteger(age) || age <= 0) {
-    res.status(400);
-    throw new Error("Age must be a positive integer");
-  }
+ const ageNumber = Number(age);
+ if (!Number.isInteger(ageNumber) || ageNumber <= 0) {
+     res.status(400);
+     throw new Error("Age must be a positive integer");
+ }
 
   // Validate required fields
   if (!age || !gender || !district || !preferred_package || !usage || !find || !type || !comment) {
